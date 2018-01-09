ccea6d0e30da09d2a02399b38e92059140d0f001
Lee Sutton <leesutton1@gmail.com>
2018-01-09 15:35:32 -0800

    Added analogRead and analogWrite functions

--- Arduino.h
+++ Arduino.h
@@ -5,12 +5,43 @@
 #include <string>
 using namespace fakeit;
 
+/**
+ * @brief Creates a mock of the Serial class available on the arduino
+ */
 struct MockSerial
 {
     virtual void print(std::string) = 0;
     virtual void println(std::string) = 0;
 };
 
+struct MockAnalog
+{
+    virtual int analogRead(int) = 0;
+    virtual void analogWrite(int, int) = 0;
+};
+
+/**
+ * Arduino requires global access to the mock objects
+ */
 fakeit::Mock<MockSerial> mock_serial;
 MockSerial& Serial = mock_serial.get();
+
+/**
+ * Global functions analogRead and analogWrite
+ */
+fakeit::Mock<MockAnalog> mock_analog;
+
+int analogRead(int value)
+{
+    MockAnalog& analog = mock_analog.get();
+    return analog.analogRead(value);
+}
+
+
+void analogWrite(int pin, int value)
+{
+    MockAnalog& analog = mock_analog.get();
+    return analog.analogWrite(pin, value);
+}
+
 #endif /* ifndef Arduino_H */
--- test.cpp
+++ test.cpp
@@ -7,9 +7,36 @@
 
 using namespace fakeit;
 
-TEST_CASE( "Loop tests", "[loop]" ) 
+TEST_CASE( "Serial class tests", "[Serial]" ) 
 {
     When(Method(mock_serial,print)).AlwaysReturn();
     When(Method(mock_serial,println)).AlwaysReturn();
-    Serial.println("testing");
+    Serial.println("testing println");
+    Serial.print("testing print");
+    Verify(Method(mock_serial, println).Using("testing println")).Once();
+    Verify(Method(mock_serial, print).Using("testing print")).Once();
+
+}
+
+TEST_CASE( "Analog read write tests", "[Analog]" ) 
+{
+    When(Method(mock_analog, analogRead)).Return(100);
+    REQUIRE(analogRead(0) == 100);
+    Verify(Method(mock_analog, analogRead).Using(0)).Once();
+
+    When(Method(mock_analog, analogWrite)).Return();
+    analogWrite(10, 20);
+    Verify(Method(mock_analog, analogWrite).Using(10, 20)).Once();
+}
+
+SCENARIO ("Mock Serial can be used in the arduino sketch") {
+    GIVEN("An arduino sketch which includes Arduino.h") {
+        WHEN("The mock tests are run") {
+            When(Method(mock_serial,print)).AlwaysReturn();
+            When(Method(mock_serial,println)).AlwaysReturn();
+            THEN("The arduino sketch can be run locally using the mock Arduino.h")
+            loop(); // Loop from main.ino
+            // TODO Verify methods have been called
+        }
+    }
 }
